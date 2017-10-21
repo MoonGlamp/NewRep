9029b54eac847d1b95271ee8c76c64f0cf07e30b
Vladislava <vlalw@ya.ru>
2017-10-22 01:12:43 +0300

    HTTP post

--- HTTP_test.py
+++ HTTP_test.py
@@ -1,2 +1,9 @@
 import requests
 
+
+def main():
+    url = 'http://cit-home1.herokuapp.com/api/register'
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(url, headers=headers, json={'FirstName': 'Vladislava', 'LastName': 'Kachalova', 'GroupId': 43506})
+    print(r.status_code, r.reason)
+    print(r.text)
